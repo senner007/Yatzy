c42696dbae1977ec0fbdfdc1f893a093c6a58401
senner007 <nielshtg@hotmail.com>
2018-02-04 17:57:57 +0100

    comments

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 "use strict";
-function OneLoopSolution(arr, name) {
+function getObject(arr, name) {
 
-    if (OneLoopSolution.cache[arr]) {
-    //    console.log('hello from cache')
-        return OneLoopSolution.cache[arr][name];
-    }
+    // if (getObject.cache[arr]) {
+    //     console.log('hello from cache')
+    //     return getObject.cache[arr][name];
+    // }
     // console.log('looping...')
 
     var pairs = [0, 0], // for arr : [1,2,2,4,1] => [2,1]
         pairCount = 0, // for arr : [1,2,2,4,1] => 2
         sum = 0,
         obj = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0,6: 0 }; //public , for arr : [1,2,2,4,1] => { 1: 2, 2: 4, 3: 0, 4: 4, 5: 0, 6: 0 }
-        
+
     for (let a of arr) {
         sum += a;
         obj[a] += a;
@@ -20,7 +20,7 @@ function OneLoopSolution(arr, name) {
         }
     }
 
-    // Betingelserne kunne også placeres i funktionerne
+    // Betingelser kunne også placeres i funktionerne
     obj.twoKind = Math.max(pairs[0], pairs[1]) * 2; // for arr : [1,2,2,4,1] => 4
     obj.threeKind = pairs[0] * 3 <= obj[pairs[0]] ? pairs[0] * 3 : pairs[1] * 3 <= obj[pairs[1]] ? pairs[1] * 3 : 0;
     // Hvis der er 3 ens, må det være af tallet pairs[0] eller pairs[1]. Check da om 3 gange en disse findes i obj
@@ -35,81 +35,83 @@ function OneLoopSolution(arr, name) {
     obj.yatzy = obj.fourKind * 5 / 4 === sum ? 50 : 0;
     obj.chance = sum;
 
-    OneLoopSolution.clearCache(); // Nye terninger
-    OneLoopSolution.cache[arr] = obj;    
+    // getObject.clearCache(); // Nye terninger
+    // getObject.cache[arr] = obj;    
     return obj[name];
 }
 
-(OneLoopSolution.clearCache = function () { 
-    OneLoopSolution.cache = {};
-})();
+// (getObject.clearCache = function () { 
+//     getObject.cache = {};
+// })();
 // Jeg antager at funktionen, med denne struktur, vil blive kaldt flere gange med det samme array som input. 
 // Derfor tilføjes et cache object på funktionen. Dette er naturligvis tiltænkt større datamængder/beregninger.
 
-console.log(ettere([1,1,2,2,1])) // looping... 3
-console.log(fullHouse([1,1,2,2,1])) // hello from cache... 7
-console.log(yatzy([1,2,3,4,5])) // looping... 0
+// console.log(ettere([1,1,2,2,1])) // looping... 3
+// console.log(fullHouse([1,1,2,2,1])) // hello from cache... 7
+// console.log(yatzy([1,2,3,4,5])) // looping... 0
 
 // Implementeret som i 'Javascript Patterns' af Stoyan Stefanov s. 76 - 'Function Properties - A Memoization Pattern'
 
+
+
 function ettere(arr) {
-    return OneLoopSolution(arr, 1)
+    return getObject(arr, 1)
 }
 
 function toere(arr) {
-    return OneLoopSolution(arr, 2)
+    return getObject(arr, 2)
 }
 
 function treere(arr) {
-    return OneLoopSolution(arr, 3)
+    return getObject(arr, 3)
 }
 
 function firere(arr) {
-    return OneLoopSolution(arr, 4)
+    return getObject(arr, 4)
 }
 
 function femmere(arr) {
-    return OneLoopSolution(arr, 5)
+    return getObject(arr, 5)
 }
 
 function seksere(arr) {
-    return OneLoopSolution(arr, 6)
+    return getObject(arr, 6)
 }
 
 function low(arr) {
-    return OneLoopSolution(arr, 'low')
+    return getObject(arr, 'low')
 }
 
 function high(arr) {
-    return OneLoopSolution(arr, 'high')
+    return getObject(arr, 'high')
 }
 
 function yatzy(arr) {
-    return OneLoopSolution(arr, 'yatzy')
+    return getObject(arr, 'yatzy')
 }
 
 function twoKind(arr) {
-    return OneLoopSolution(arr, 'twoKind')
+    return getObject(arr, 'twoKind')
 }
 
 function threeKind(arr) {
-    return OneLoopSolution(arr, 'threeKind')
+    return getObject(arr, 'threeKind')
 }
 
 function fourKind(arr) {
-    return OneLoopSolution(arr, 'fourKind')
+    return getObject(arr, 'fourKind')
 }
 
 function twoPairs(arr) {
-    return OneLoopSolution(arr, 'twoPairs')
+    return getObject(arr, 'twoPairs')
 }
 
 function fullHouse(arr) {
-    return OneLoopSolution(arr, 'fullHouse')
+    return getObject(arr, 'fullHouse')
 }
 
 function chance(arr) {
-    return OneLoopSolution(arr, 'chance')
+    return getObject(arr, 'chance')
 }
 
 
